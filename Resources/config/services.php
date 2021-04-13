a0faedcd3fa71c636999851af454591c11da50e7
Karoly Gossler <connor@connor.hu>
2021-04-13 16:05:00 +0200

    added some backward compatibility

--- Resources/config/services.php
+++ Resources/config/services.php
@@ -7,14 +7,24 @@ use KHTools\VPosBundle\VPosClientProviderInterface;
 use Psr\Http\Client\ClientInterface;
 
 return static function (ContainerConfigurator $container) {
+    $args = [];
+
+    if (function_exists('param')) {
+        $args = [
+            param('khvpos.vposclient_provider.config'),
+            service(ClientInterface::class)
+        ];
+    }
+    else {
+        $args = [
+            '%khvpos.vposclient_provider.config%',
+            service(ClientInterface::class),
+        ];
+    }
+
     $container->services()
         ->set('khvpos.vposclient_provider', VPosClientProvider::class)
-            ->args([
-                param('khvpos.vposclient_provider.config'),
-                service(ClientInterface::class)
-            ])
+            ->args($args)
             ->tag('container.hot_path')
         ->alias(VPosClientProviderInterface::class, 'khvpos.vposclient_provider');
-
-
 };
